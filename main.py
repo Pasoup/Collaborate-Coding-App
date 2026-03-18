3771595d01842cea24f1dba54f9677e9e8f3068c
Pasoup <pasuleela@gmail.com>
2026-03-18 21:06:28 +0700

    Updated server, login (should) work

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget
-
-
+import requests
 from output import Ui_Form 
+
+url = "https://collaborate-coding-app.onrender.com"
 
 class MyWorkshopApp(QWidget):
     def __init__(self):
@@ -16,23 +17,55 @@ class MyWorkshopApp(QWidget):
         self.ui.loginRegisterBtn.clicked.connect(self.changeToRegister)
         self.ui.registerLoginBtn.clicked.connect(self.changeToLogin)
 
+        self.ui.registerConfirmBtn.clicked.connect(self.register)
+        self.ui.loginConfirmBtn.clicked.connect(self.login)
     def changeToRegister(self):
         self.ui.MainPages.setCurrentIndex(1)
 
     def changeToLogin(self):
         self.ui.MainPages.setCurrentIndex(0)
 
+    def register(self):
+        username = self.ui.registerUsername.toPlainText().strip()
+        passwrod = self.ui.registerPassword.toPlainText().strip()
+        confirm_password = self.ui.registerConfirmPassword.toPlainText().strip()
+        print(f"DEBUG: Comparing '{passwrod}' with '{confirm_password}'")
+        if passwrod != confirm_password:
+            print("Passsword not the same")
+            return
+
+        try:
+            url = "https://collaborate-coding-app.onrender.com/register"
+            response = requests.post(url, json={"username": username, "password": passwrod})
+
+            if response.status_code == 200:
+                print("Register sucessful!")
+                self.changeToLogin()
+            else:
+                print("Register failed!")
+        except Exception as e:
+            print("could not connect to server")
+    
     def login(self):
         username = self.ui.loginUsername.toPlainText().strip()
-        passwrod = self.ui.loginPassword.toPlainText().strip()
-        
+        password = self.ui.loginPassword.toPlainText().strip()
+        try:
+            url = "https://collaborate-coding-app.onrender.com/login"
+            response = requests.post(url, json={"username": username, "password": password})
+            if response.status_code == 200:
+                print(f"Login sucess for , {username}!")
+                self.ui.MainPages.setCurrentIndex(2) 
+                self.ui.homeUserName.setText(f"{username}'s Dashboard") 
+            else:
+                print("Login failed")
+        except Exception as e:
+            print("could not connect to server")
+
+
 
 if __name__ == "__main__":
    
     app = QApplication(sys.argv)    
-
     window = MyWorkshopApp()
     window.show()
-    
-    
     sys.exit(app.exec())
